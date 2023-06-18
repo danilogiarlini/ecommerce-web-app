30868a1dad5f840e011e7fc89b7e2c1b8aa0f0c8
Danilo Giarlini <danilogiarlini@gmail.com>
2023-06-18 19:05:15 +0200

    created model

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,9 +1,30 @@
+
+import { Product } from '@/model/product';
+import PocketBase from 'pocketbase';
+import { useEffect, useState } from 'react';
+
+export const pb = new PocketBase('http://127.0.0.1:8090');
+
 export function ShopPage() {
-    return (
-      <div>
-        <h1 className="title">SHOP</h1>
-  
-        content here
-      </div>
-    )
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  function loadData() {
+    pb.collection('products').getList<Product>()
+      .then(res => {
+        setProducts(res.items)
+      })
   }
+
+  console.log(products)
+  return (
+    <div>
+      <h1 className="title">SHOP</h1>
+
+      <pre>{JSON.stringify(products, null, 2)}</pre>
+    </div>
+  )
+}
