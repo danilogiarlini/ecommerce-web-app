4736a58d7d51175eac7f7f9c580cca572e2213c6
Danilo Giarlini <danilogiarlini@gmail.com>
2023-06-19 13:36:56 +0200

    set environment files

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,30 +1,38 @@
-
 import { Product } from '@/model/product';
-import PocketBase from 'pocketbase';
 import { useEffect, useState } from 'react';
+import { pb } from '../../pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
 
 export function ShopPage() {
   const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
-    loadData()
-  }, [])
-
-  function loadData() {
     pb.collection('products').getList<Product>()
       .then(res => {
         setProducts(res.items)
       })
-  }
+  }, [])
 
-  console.log(products)
   return (
     <div>
       <h1 className="title">SHOP</h1>
 
-      <pre>{JSON.stringify(products, null, 2)}</pre>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-16'>
+      {
+        products.map(p => {
+          return (
+            <div key={p.id}>
+              <img src={p.img} alt={p.name} />
+              {p.name}
+              euro {p.cost}
+
+              {p.description}
+            </div>
+          )
+        })
+      }
+      </div>
+
     </div>
   )
 }
