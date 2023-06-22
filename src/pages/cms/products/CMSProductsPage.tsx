c6405161ea8c43c2d2b0aa756702f3106dcdb961
Danilo Giarlini <danilogiarlini@gmail.com>
2023-06-22 10:49:37 +0200

    products list component

--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,71 +1,30 @@
-import { useProductsService } from "@/services/products";
-import { useEffect } from "react";
-import { ServerError } from "../../../shared/";
-import clsx from "clsx";
+import { useEffect } from 'react';
+import { useProductsService } from '@/services/products';
+import { ServerError, Spinner } from '@/shared/';
+import { CMSProductsList } from './components/CMSProductsList';
 
 export function CMSProductsPage() {
   const { state, actions } = useProductsService();
 
   useEffect(() => {
-    actions.getProducts();
-  }, []);
+    actions.getProducts()
+  }, [])
 
   return (
     <div>
       <h1 className="title">CMS</h1>
 
-      {state.pending && <div>loading...</div>}
+      {state.pending && <Spinner />}
       {state.error && <ServerError message={state.error} />}
 
-      <div className="mt-12">
-        <table className="table-auto w-full hover">
-          <thead>
-            <tr>
-              <th className="text-left">PRODUCTS</th>
-              <th className="text-left">IMAGE</th>
-              <th>COST</th>
-              <th>DELETE</th>
-            </tr>
-          </thead>
+      <CMSProductsList
+        items={state.products}
+        activeItem={state.activeItem}
+        onEditItem={actions.setActiveItem}
+        onDeleteItem={actions.deleteProduct}
+      />
 
-          <tbody>
-            {state.products.map((item) => {
-              return (
-                <tr
-                  key={item.id}
-                  className={clsx("clsxcursor-pointer", { "bg-sky-200 text-black pointer-events-none": item.id === state.activeItem?.id })}
-                  onClick={() => {
-                    actions.setActiveItem(item);
-                  }}
-                >
-                  <td>{item.name}</td>
-                  <td>
-                    {" "}
-                    {item.tmb && (
-                      <img
-                        src={item.tmb}
-                        alt={item.name}
-                        className="h-16 rounded-xl"
-                      />
-                    )}
-                  </td>
-                  <td className="text-center">€ {item.cost}</td>
-                  <td className="text-center">
-                    <i
-                      className="fa fa-trash btn danger"
-                      onClick={(e) => {
-                        e.stopPropagation()
-                        actions.deleteProduct(item.id);
-                      }}
-                    ></i>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
       <pre>{JSON.stringify(state.activeItem, null, 2)}</pre>
     </div>
-  );
+  )
 }
